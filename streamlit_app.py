14e221d32295db4966fe576933e06eae0e7f93d1
fslopezTuring14 <134466267+fslopezTuring14@users.noreply.github.com>
2023-05-23 21:25:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,8 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
